5482e76ca6301bb7bcb3b1087851d46b852e82ca
Andrius <asatraitis@gmail.com>
2022-04-09 10:06:50 -0400

    Add schema w/ getItems resolver; Init mercurius

---
+++ apps/api/src/Item/index.js
@@ -0,0 +1,1 @@
+export * as itemQueries from './query.js';
---
+++ apps/api/src/Item/query.js
@@ -0,0 +1,14 @@
+import { GraphQLList } from 'graphql';
+import Item from './Item.js';
+import prisma from '../prismaClient.js';
+
+export const getItems = {
+  type: new GraphQLList(Item),
+  resolve: async () => {
+    try {
+      return prisma.item.findMany();
+    } catch (err) {
+      throw new Error(err);
+    }
+  },
+};
