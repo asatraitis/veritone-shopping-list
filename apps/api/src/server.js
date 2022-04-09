7db5c224231ed8acd87a3da982dc961ece96cf23
Andrius <asatraitis@gmail.com>
2022-04-09 09:00:50 -0400

    Add eslint; prettier; update project formatting

--- apps/api/src/server.js
+++ apps/api/src/server.js
@@ -1,23 +1,23 @@
 import Fastify from 'fastify';
 
 export default function server() {
-	const fastify = Fastify({
-		logger: true,
-	});
+  const fastify = Fastify({
+    logger: true,
+  });
 
-	fastify.get('/api', function (_request, reply) {
-		try {
-			reply.send({ status: 'ok' });
-		} catch (err) {
-			throw new Error(err);
-		}
-	});
+  fastify.get('/api', function (_request, reply) {
+    try {
+      reply.send({ status: 'ok' });
+    } catch (err) {
+      throw new Error(err);
+    }
+  });
 
-	fastify.listen(3005, function (err, address) {
-		if (err) {
-			fastify.log.error(err);
-			process.exit(1);
-		}
-		console.log(`Server is up: ${address}`);
-	});
+  fastify.listen(3005, function (err, address) {
+    if (err) {
+      fastify.log.error(err);
+      process.exit(1);
+    }
+    console.log(`Server is up: ${address}`);
+  });
 }
