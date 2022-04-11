17caa08cc6f1e5e9a3f63912eae0947cf8cfb7ce
Andrius <asatraitis@gmail.com>
2022-04-11 09:20:17 -0400

    Add DeleteModal

---
+++ apps/client/src/components/DeleteModal/propTypes.js
@@ -0,0 +1,7 @@
+import PropTypes from 'prop-types';
+
+export const deleteItemModalPropTypes = {
+  open: PropTypes.bool,
+  onClose: PropTypes.func,
+  onConfirm: PropTypes.func,
+};
--- apps/client/src/components/index.jsx
+++ apps/client/src/components/index.jsx
@@ -4,3 +4,4 @@ export { default as Button } from './Button';
 export { default as ListItem } from './ListItem';
 export { default as ItemModal } from './ItemModal';
 export { default as SelectAmount } from './SelectAmount';
+export { default as DeleteModal } from './DeleteModal';
