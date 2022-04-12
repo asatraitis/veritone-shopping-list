d7d47118ae1e0a38875b9e9c51f093f84ca69af9
Andrius <asatraitis@gmail.com>
2022-04-11 20:01:31 -0400

    Update scripts; misc

--- apps/client/src/components/AppBar/AppBar.jsx
+++ apps/client/src/components/AppBar/AppBar.jsx
@@ -2,7 +2,11 @@ import Box from '@mui/material/Box';
 import { appBarPropTypes } from './propTypes';
 import { appBarStyles } from './styles';
 
-const AppBar = ({ title }) => <Box sx={appBarStyles}>{title}</Box>;
+const AppBar = ({ title }) => (
+  <Box component="header" sx={appBarStyles}>
+    {title}
+  </Box>
+);
 
 AppBar.propTypes = appBarPropTypes;
 AppBar.defaultProps = {
